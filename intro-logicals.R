d280fa385ec596ba17d388f66416791a5f3b436f
kylienewcomer <kylienewcomer@ucsb.edu>
2025-08-08 13:53:39 -0700

    logical practice

---
+++ intro-logicals.R
@@ -0,0 +1,36 @@
+rm(list= ls())
+
+dogs <- c(1,10,4,5,18)
+cats <- c(0,12,10,5,2)
+
+dogs < cats
+dogs <= cats
+
+dogs == cats
+
+5 == TRUE
+
+1 == TRUE
+
+0 == FALSE
+
+5 != 6
+
+"cat" <= "dog"
+
+#logical and's
+
+5<10 & "cat" < "dog"
+
+9>4 | "dog" < "cat"
+
+bananas <- c(10,21,12,15,22)
+apples <- c(9,18,16,20,30)
+
+apples < bananas & bananas  > 20
+
+apples < bananas | bananas  > 20
+
+4 < 10 | 100 < 2
+
+5 > 6 | "dog" < "cat" | 10<50
